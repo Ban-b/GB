94defffc9d905f4000858a21d954a8426f382f96
Ban-b <84246828+Ban-b@users.noreply.github.com>
2022-02-22 20:23:02 +0300

    Update диаметр и длину окружности.go

--- диаметр и длину окружности.go
+++ диаметр и длину окружности.go
@@ -5,10 +5,17 @@ import (
 )
 
 func main() {
-  const pi = 3.14;
-	var R, S, l int
+  const pi = 3.14 int
+  	var R, S, l, D int
 
 	fmt.Print("Введите площадь окружности: ")
-	fmt.Scanln(&a)
-
+	fmt.Scanln(&S)
+	
+  R = 2 * S / pi
+	
+  D = math.Sqrt(R)
+	fmt.Println("Диаметр окружности: " + D)	
   
+  l = pi * D
+	fmt.Println("Длина окружности: " + l)
+}	
